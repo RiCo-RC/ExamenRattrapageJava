54469cbaf2810971d9186e12c3b15963d3f0a0ee
Christopher Rieffel <christopher.rieffel@uha.fr>
2025-01-27 11:47:22 +0100

    v.0.0.3

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,134 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Calculator2 {
+public class Calculator {
+
+    public String[] numberList = new String[10];
+    public String[] operatorList = new String[5];
+
+    public String operatorType = "";
+    public String expression = "";
+
+    public List<Double> expressionNumber = new ArrayList<Double>();
+    public List<Character> expressionOperator = new ArrayList<Character>();
+
+    public double result = 0.0;
+
+    public void buttons() {
+        numberList[0] = "0";
+        numberList[1] = "1";
+        numberList[2] = "2";
+        numberList[3] = "3";
+        numberList[4] = "4";
+        numberList[5] = "5";
+        numberList[6] = "6";
+        numberList[7] = "7";
+        numberList[8] = "8";
+        numberList[9] = "9";
+
+        operatorList[0] = "+";
+        operatorList[1] = "-";
+        operatorList[2] = "*";
+        operatorList[3] = "/";
+        operatorList[4] = "%";
+    }
+
+    public Calculator clickOnButton(String buttonValue, Boolean displayInput) {
+        for (char character : buttonValue.toCharArray()) {
+            if (!Character.isDigit(character)) {
+                this.operatorType = String.valueOf(character);
+            }
+            if (expression.isEmpty()) expression = String.valueOf(character);
+            else expression += String.valueOf(character);
+        }
+        if (displayInput) System.out.println("--> " + this.expression);
+        return this;
+    }
+
+    public Calculator extractNumber() {
+        StringBuilder number = new StringBuilder();
+        for (char character : this.expression.toCharArray()) {
+            if (Character.isDigit(character)) {
+                number.append(character);
+            } else {
+                this.expressionNumber.add(Double.parseDouble(number.toString()));
+                number.setLength(0);
+            }
+        }
+        this.expressionNumber.add(Double.parseDouble(number.toString()));
+        //System.out.println("--> " + this.expressionNumber);
+        return this;
+    }
+
+    public Calculator extractOperator() {
+        for (char character : this.expression.toCharArray()) {
+            if (character == '+' || character == '-' || character == '*' || character == '/') {
+                this.operatorType = String.valueOf(character);
+                this.expressionOperator.add(character);
+            }
+        }
+        //System.out.println("--> " + this.expressionOperator);
+        //System.out.println("--> " + this.operatorType);
+        return this;
+    }
+
+    public Calculator handlePriority() {
+        for (int i = 0; i < this.expressionOperator.size(); i++) {
+            this.operatorType = String.valueOf(this.expressionOperator.get(i));
+            if (this.operatorType.equals("*")) {
+                this.result = this.expressionNumber.get(i) * this.expressionNumber.get(i + 1);
+            }
+            if (this.operatorType.equals("/")) {
+                this.result = this.expressionNumber.get(i) / this.expressionNumber.get(i + 1);
+            }
+            if (this.operatorType.equals("*") || this.operatorType.equals("/")) {
+                //this.expressionNumber.set(i, this.result);
+                this.expressionNumber.remove(i + 1);
+                this.expressionNumber.remove(i);
+                this.expressionNumber.add(this.result);
+                this.expressionOperator.remove(i);
+                i++;
+            }
+        }
+        return this;
+    }
+
+    public Calculator extractResult() {
+        this.result = this.expressionNumber.getFirst();
+        for (int i = 0; i < this.expressionOperator.size(); i++) {
+            this.operatorType = String.valueOf(this.expressionOperator.get(i));
+            if (this.operatorType.equals("+")) {
+                this.result += this.expressionNumber.get(i + 1);
+            }
+            if (this.operatorType.equals("-")) {
+                this.result -= this.expressionNumber.get(i + 1);
+            }
+        }
+        return this;
+    }
+
+    public Calculator calculate() {
+        this.extractNumber();
+        this.extractOperator();
+        this.handlePriority();
+        this.extractResult();
+        System.out.println("Result is: " + this.result);
+        return this;
+    }
+
+    public Calculator reset() {
+        this.operatorType = "";
+        this.expression = "";
+
+        this.expressionNumber = new ArrayList<Double>();
+        this.expressionOperator = new ArrayList<Character>();
+        this.result = 0.0;
+
+        return this;
+    }
+
+    public Calculator divider() {
+        System.out.println("----------");
+        return this;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,60 @@ public class Main {
 
         //-- LOOP --\\
 
-        calculator.clickOnButton("5").clickOnButton("+").clickOnButton("5").calculate();
-        calculator.clickOnButton("2").clickOnButton("-").clickOnButton("2").calculate();
+        // addition (5+5 = 10)
+        calculator.
+                clickOnButton("5", true).
+                clickOnButton("+", true).
+                clickOnButton("5", true).
+                calculate().
+                reset().
+                divider();
+
+        // addition (2+2=4) without input showing
+        calculator.
+                clickOnButton("2", false).
+                clickOnButton("+", false).
+                clickOnButton("2", false).
+                calculate().
+                reset().
+                divider();
+
+        // subtraction (5-5=0)
+        calculator.
+                clickOnButton("5", true).
+                clickOnButton("-", true).
+                clickOnButton("5", true).
+                calculate().
+                reset().
+                divider();
+
+        // multiplication (2*2=4)
+        calculator.
+                clickOnButton("2", true).
+                clickOnButton("*", true).
+                clickOnButton("2", true).
+                calculate().
+                reset().
+                divider();
+
+        // division (10/2 = 5)
+        calculator.
+                clickOnButton("10", true).
+                clickOnButton("/", true).
+                clickOnButton("2", true).
+                calculate().
+                reset().
+                divider();
+
+        // (2+(2*4)) -> 2+8 -> 10
+        calculator.
+                clickOnButton("2", true).
+                clickOnButton("+", true).
+                clickOnButton("2", true).
+                clickOnButton("*", true).
+                clickOnButton("4", true).
+                calculate().
+                reset().
+                divider();
     }
 }
